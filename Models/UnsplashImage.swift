4f177377096f1d057820aadd7cb3e5b6e69d33ee
Praniket <entrapraniour@gmail.com>
2025-02-19 12:26:58 +0530

    Update UnsplashImage.swift

--- Models/UnsplashImage.swift
+++ Models/UnsplashImage.swift
@@ -1,1 +1,18 @@
+import Foundation
+
+struct UnsplashImage: Codable, Identifiable {
+    let id: String
+    let urls: ImageURLs
+    let user: User
+    let description: String?
+
+    struct ImageURLs: Codable {
+        let small: String
+        let regular: String
+    }
+
+    struct User: Codable {
+        let name: String
+    }
+}
 
